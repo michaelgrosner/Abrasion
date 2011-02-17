3a44fa50777de4fffbd7cf95769d45494b70cd7c
Michael Grosner <michaelgrosner@gmail.com>
2011-02-17 00:32:06 -0500

    Version 1.01

--- Abrasion2.py
+++ Abrasion2.py
@@ -1,9 +1,97 @@
 from BeautifulSoup import *
-import urllib2, datetime, re
+import urllib2, datetime, re, json, sys, getopt
 
-""" """
+""" abrasionSearch is a very small, free, command-line interface to searching
+	1. Google
+	2. Bing
+	3. Twitter
+	4. Yahoo!
+	5. Blekko
+	6. Ask
+7. (not implemented yet) DuckDuckGo
 
-def webSearch(search_term, engine='google', site=False, nresults=10):
+Provide a search term and the maximum number of results desired, and Abrasion will
+use any or all of the above search engines to return a list of links. That is all.
+
+Dependencies:
+1. Python 2.6-2.7 (tested on Mac OS X and Ubuntu)
+2. BeautifulSoup (www.crummy.com/software/BeautifulSoup/) placed in either your
+   PYTHONPATH or in the same directory as abrasionSearch
+
+Usage:
+1. From command-line:
+  > python abrasion.py "search phrase" --[options]=[value]
+2. From python scripts/interpreter
+	> import abrasion
+	> [return list] = abrasion.Search('search phrase', [options]=[value], )
+
+Options:
+  --engine=    Search engine to use. Defaults to 'google'. Also supports:
+               'bing', 'yahoo', 'twitter', 'ask', 'blekko'
+  --nresults=  Number of results to serve back. Defaults to 10. Twitter API maxes
+	             out at 100.
+	--site=      Search within a specific domain i.e. search all "Google" articles on
+	             'engadget.com'. Not supported on 'twitter'.
+	-f           Do not follow bit.ly, goo.gl, etc. redirect links.
+
+Written by Michael Grosner
+Feb 16, 2011
+Use freely; add, fork, include in your work, etc.
+"""
+
+def twitterSearch(search_term, nresults=80, followLinks=True, output='p'):
+	if nresults > 100: raise Exception('Please specify less than 100 results')
+	
+	# As before, change our user agent to prevent blocking, although with Twitter
+	# I suspect they are much more liberal than the web searchers
+	opener = urllib2.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
+	# Construct a URL based on documentation found at...
+	# http://apiwiki.twitter.com/w/page/22554756/Twitter-Search-API-Method:-search
+	url = 'http://search.twitter.com/search.json?q=%s&rpp=%i' % (search_term, nresults)
+
+	# Try and grab the JSON formatted data
+	try:	
+		twitterData = json.load(opener.open(url))
+	except:
+		print 'Cannot load twitter data on %s' % search_term
+	
+	linkList = []
+	for tweet in twitterData['results']:
+		
+		# For every tweet, use a regex to grab the link component
+		text = tweet['text']
+		link = re.compile("https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?").search(text)
+
+		# Not every tweet will yield a link, so don't cause an error if no link is found.
+		# If there is a link, the regex is screwy and so the 0th and 2nd parts need to be
+		# rejoined
+		try:
+			lg = link.groups()
+			url = str(''.join([lg[0], lg[2]]))
+			
+			# If the user has indicated to follow all links, and not just get the bit.ly
+			# etc links, use the .geturl() method to find the followed URL
+			if followLinks:
+				r = opener.open('http://'+url)
+				linkList.append( r.geturl() )
+			else:	
+				linkList.append( url )
+				
+		except:
+			pass
+	
+	# Depending on whether this is used through the command-line interface or plugged
+	# directly into python code, change the output. I would rather print out the linkList
+	# in this manner so it'd be easier to run on a pipe for further processing.
+	if output == 'p':
+		for l in linkList:
+			print l
+	elif output == 'r':
+		return linkList
+
+def webSearch(search_term, engine='google', site=False, nresults=10, output='p'):
 	
 	# Data structure containing the procedure to determining links
 	engine_rules = {'google': ['http://www.google.com/search?q=',          # Base URL for search engine
@@ -55,27 +143,71 @@ def webSearch(search_term, engine='google', site=False, nresults=10):
 	i = 0
 	while len(linkList) < nresults:
 		try:
-			if i != 0:	
+			# If the first page, don't include the page numbering, yet.
+			if i != 0:
+				# engine[0] rule is the base URL
+				# engine[4](i) is the lambda function which maps the page number to
+				#   the incrementor used in the URL
 				url = '%s%s%s' % (engine_rules[engine][0], search_term, engine_rules[engine][4](i))
 			else:
 				url = '%s%s' % (engine_rules[engine][0], search_term)
-			print url
+			response = opener.open(url)
 		except:
 			print 'Could not download search for %s at %s' % (search_term,url)
-			
-		response = opener.open(url)
 		
 		# Join all the response HTML together and send it to BeautifulSoup to work it's magic
 		soup = BeautifulSoup(''.join(response))
+		
+		# Using the 1st and 2nd rules, use BeautifulSoup to parse the HTML and pick out
+		# the links we are so desirous of.
 		newLinkList = soup.findAll(attrs={engine_rules[engine][1]: engine_rules[engine][2]})
 		
+		# Using the 3rd rule, a lambda function which contains all the directions to
+		# taking the BeautifulSoup HTML parsed <a href=...>mfcsdce</a> and cut it down to
+		# just the URL
 		linkList.extend( map(engine_rules[engine][3], newLinkList) )
 		i += 1
 	
+	# Trim down the linkList to the desired number of results
 	linkList = linkList[:nresults]
 	
-	return linkList
+	# As in the Twitter search, output in different ways.
+	if output == 'p':
+		for l in linkList:
+			print l
+	elif output == 'r':
+		return linkList
+
+# This function is the entry point for all searches, and will either send the search
+# to Twitter or web-based searches
+def Search(search_term, engine='google', site=False, followLinks=True, nresults=10, output='r'):
+	
+	if engine == 'twitter':
+		twitterSearch(search_term, nresults, followLinks, output)
+	elif engine in ['google', 'yahoo', 'bing', 'blekko', 'ask']:
+		webSearch(search_term, engine, site, nresults, output)
+	
+# This function handles the command-line arguments, passing them to Search()
+def searchFromBash():
+	search_term = sys.argv[1]
+	opts, args = getopt.getopt(sys.argv[2:], 'f', ['engine=', 'nresults=', 'site='])
+	
+	engine = 'google'
+	site = False
+	nresults = 10
+	followLinks = True
+	
+	for o, a in opts:
+		if   o == '--engine':
+			engine = a
+		elif o == '-f':
+			followLinks=False
+		elif o == '--nresults':
+			nresults = a
+		elif o == '--site':
+			site=site
+		
+	Search(search_term, engine=engine, site=site, followLinks=followLinks, nresults=nresults, output='p')
 
 if __name__ == '__main__':
-	ws = webSearch('reddit.com', engine='google', nresults=20)
-	print ws
+	searchFromBash()
